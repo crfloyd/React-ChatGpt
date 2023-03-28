a3dcc7caee79efedd8a7ffa1e6935f30df763fd1
corey <fourfloyds@gmail.com>
2023-03-28 00:40:15 -0700

    adds integration with openai in backend

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 // OpenAI configuration
 const configuration = new Configuration({
-	apiKey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.OPEN_API_KEY,
 });
 export const openai = new OpenAIApi(configuration);
 
--- server/routes/openai.js
+++ server/routes/openai.js
@@ -8,11 +8,34 @@ const router = express.Router();
 
 router.post("/text", async (req, res) => {
 	try {
-		const { text, activeChatId } = req.body;
-		console.log("text", text);
-		res.status(200).json({ text });
+		const { text, id } = req.body;
+
+		const response = await openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages: [
+				{ role: "system", content: "You are a helpful assistant." }, // this represents the bot and what role they will assume
+				{ role: "user", content: text }, // the message that the user sends
+			],
+		});
+
+		const aiResponseText = response.data.choices[0].message.content;
+		console.log("openAI response:", aiResponseText);
+
+		await axios.post(
+			`https://api.chatengine.io/chats/${id}/messages/`,
+			{ text: aiResponseText },
+			{
+				headers: {
+					"Project-ID": process.env.PROJECT_ID,
+					"User-Name": process.env.BOT_USER_NAME,
+					"User-Secret": process.env.BOT_USER_SECRET,
+				},
+			}
+		);
+
+		res.status(200).json({ text: response.data.choices[0].message.content });
 	} catch (error) {
-		console.error(error);
+		console.error("error", error);
 		res.status(500).json({ error: error.message });
 	}
 });
